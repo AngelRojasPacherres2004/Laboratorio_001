beef6db98494bb408b752f97db48fbe569ad15bc
AngelRojasPacherres2004 <142623592+AngelRojasPacherres2004@users.noreply.github.com>
2023-08-18 23:53:16 -0500

    Add files via upload

---
+++ src/laboratorio_001/Laboratorio_001.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio_001;
+
+/**
+ *
+ * @author ferna
+ */
+public class Laboratorio_001 {
+
+//TODO ESTA EN LA CLASE PERSONA<----------
+    
+}
